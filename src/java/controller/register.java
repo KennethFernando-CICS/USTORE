8c738293bfc472a9297e9336b8e8fdd536e0ee4a
KennethFernando-CICS <kennethchristopher.fernando.cics@ust.edu.ph>
2022-12-07 18:55:32 +0800

    Added Stuff to Map and List

--- src/java/controller/register.java
+++ src/java/controller/register.java
@@ -1,21 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
 
 import java.io.*;
+import java.util.*;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.User;
 
-/**
- *
- * @author LENOVO
- */
 public class register extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -23,17 +17,20 @@ public class register extends HttpServlet {
         String user = request.getParameter("username");
         String pass = request.getParameter("password");
         ServletContext context = getServletContext();
-        String path = context.getRealPath("") + "/WEB-INF/";
+        Map<String, String> credentials = (Map) context.getAttribute("credentials");
+        List<User> userList = (ArrayList) context.getAttribute("userList");
         
         try ( PrintWriter out = response.getWriter()) {
-            File reg = new File(path);
-            FileWriter writer = new FileWriter(reg, true);
-            writer.write(user);
-            writer.write(pass);
-            writer.close();
+            credentials.put(user, pass);
+            User u = new User(user, pass);
+            u.setFirstName(request.getParameter("fname"));
+            u.setLastName(request.getParameter("lname"));
+            u.setUsername(user);
+            u.setPassword(pass);
+            u.setEmail(request.getParameter("email"));
+            userList.add(u);
             //pass username password to login servlet
             request.getSession().setAttribute("username", user);
-            request.getSession().setAttribute("login", "true");
             RequestDispatcher view = request.getRequestDispatcher("index.jsp");
             view.include(request, response);
         }
@@ -41,7 +38,6 @@ public class register extends HttpServlet {
             response.sendError(440);
         }
     }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -80,5 +76,4 @@ public class register extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
